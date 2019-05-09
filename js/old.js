05e74c2b343544b108d741ed9f7593901cb1a52f
ChrisLaRocque <larocque.christopher@gmail.com>
2019-05-09 13:35:29 -0400

    Beginning of teaser page

--- js/old.js
+++ js/old.js
@@ -3,7 +3,8 @@ $('.video-cover').click(function(){
     var id =  $(this).attr('id');
     var title = $(this).find('h4').text()
     
-    $('.video-cover').html('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + id + '?rel=0&autoplay=1"></iframe></div><div class="row"><h2 class="p-2 light header title">' + title + '</h2></div>');
+    $('.featured-video').show();    
+    $('.featured-video').html('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + id + '?rel=0&autoplay=1"></iframe></div><div class="row"><h2 class="p-2 light header title">' + title + '</h2></div>');
 }); 
 
 /* Countdown Timer */
